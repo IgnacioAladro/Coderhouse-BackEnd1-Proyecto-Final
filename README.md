180068a904a69039d691f9ccbbefa96e395cdebf
nachoaladro <ignacioaladro385@gmail.com>
2024-05-24 22:38:09 -0300

    Handlebars listo

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 
-# CoderHouse // BackEnd // Primera Pre Entrega
+# CoderHouse // BackEnd I // Segunda Pre Entrega
 
-El trabajo desarrola un servidor que contiene los endpoints y servicios necesarios para gestionar los productos y carritos de compra de un e-commerce
+El trabajo desarrola un servidor que contiene los endpoints y servicios necesarios para gestionar los productos y carritos de compra de un e-commerce.
+Ademas, configura el servidor para integrar el motor de plantillas Handlebars e instala un servidor de socket.io al mismo.
 
 
 ## Authors
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,9 +1,24 @@
 import { Router } from "express";
-
 const router = Router();
 
+import { __dirname } from '../path.js';
+
+import { promises as fs } from 'fs';
+
+const loadProducts = async () => {
+    try {
+        const data = await fs.readFile(`${__dirname}/manager/products.json`, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        res.status(400).send('No se pueden cargar los productos')
+        console.log(error);
+    }
+};
+
+const allProducts = await loadProducts();
+
 router.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', { products: allProducts });
 });
 
 export default router;
